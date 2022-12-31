5e9599432da8fce35416b2f54cd43500347ed757
poncho678 <vincent.tollens@googlemail.com>
2022-12-31 17:55:30 +0100

    feat: single Projects display loader if there is no data

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,11 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Loader from "../../components/Loader";
 export default function project() {
   const router = useRouter();
   const { slug } = router.query;
 
+  const [projectData, setProjectData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // load Single Project Data
+
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <>
+      <h1>Single Project</h1>
       <h1>{slug}</h1>
       <Link href="/">Home</Link>
     </>
