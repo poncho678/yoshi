77531986800272e6b1294d75a4ff82a0e3c563eb
poncho678 <vincent.tollens@googlemail.com>
2023-01-08 10:45:13 +0100

    moving files

--- components/Home/ProjectlistItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./ProjectlistItem.module.css";
-
-function ProjectlistItem({ project }) {
-  return <article className={styles.project}>Projectlistitem</article>;
-}
-
-export default ProjectlistItem;
--- components/Home/Slider.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Slider.module.css";
-
-function Slider() {
-  return <div>Slider</div>;
-}
-
-export default Slider;
