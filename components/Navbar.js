e79495dc6bd99614890ec8ab8b64cbfdd663b37a
poncho678 <vincent.tollens@googlemail.com>
2023-01-08 10:46:39 +0100

    fix: Navbar now is a component, duh

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,20 @@ import Link from "next/link";
 import React from "react";
 import styles from "./Navbar.module.css";
 
-function navbar() {
+function Navbar() {
   return (
-    <nav className={styles.nav}>
-      <Link href="/">Jascha Hannover</Link>
-      <ul className={styles.linkWrapper}>
-        <li>About</li>
-        <li>Work</li>
-        <li>Bio</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <header>
+      <nav className={styles.nav}>
+        <Link href="/">Jascha Hannover</Link>
+        <ul className={styles.linkWrapper}>
+          <li>About</li>
+          <li>Work</li>
+          <li>Bio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
-export default navbar;
+export default Navbar;
