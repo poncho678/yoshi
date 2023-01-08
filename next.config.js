6d40871eb1fe477c2ecafcc210b611c0d179883c
poncho678 <vincent.tollens@googlemail.com>
2023-01-08 10:46:03 +0100

    connecting to db

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["cdn.sanity.io", "themoviedb.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
