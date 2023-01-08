b0de5505437e7ba9fe7471e0ffcd699ed8b1f531
poncho678 <vincent.tollens@googlemail.com>
2023-01-08 10:47:37 +0100

    importing navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="appContainer">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
