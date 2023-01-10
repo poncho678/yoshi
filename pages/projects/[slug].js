b497fa645c709d2541bd34de0dca915bb9cd2533
poncho678 <vincent.tollens@googlemail.com>
2023-01-10 19:22:50 +0100

    created single page

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,18 +1,58 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { getClient } from "../../server/sanity.server";
+import { getClient, urlFor } from "../../server/sanity.server";
 
 import Loader from "../../components/Loader";
 import { queryAllPosts } from "../../server/sanity.queries";
+import dynamic from "next/dynamic";
+import Synopsis from "../../components/Synopsis";
+import Stills from "../../components/singleProject/Stills";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 function Project({ project }) {
-  const { title, stills, trailer } = project;
+  const {
+    title,
+    client,
+    stills,
+    poster,
+    trailer,
+    runtime,
+    releaseDate,
+    worldSales,
+    synopsis,
+    team,
+    festivals,
+  } = project;
+
+  console.log(project);
+  console.log(stills);
+
   return (
-    <>
-      <h1>{title}</h1>
-      <Link href="/">Home</Link>
-    </>
+    <main>
+      <header>
+        {title && <h1>{title}</h1>}
+        {client && <h3>{client}</h3>}
+      </header>
+      <section>
+        {poster && (
+          <Image
+            src={urlFor(poster).url()}
+            alt=""
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
+        )}
+      </section>
+      {trailer && <ReactPlayer url={trailer} controls={true} />}
+      {synopsis.length > 0 && (
+        <section>
+          <Synopsis synopsis={synopsis} />
+        </section>
+      )}
+      {stills.length > 0 && <Stills stills={stills} />}
+    </main>
   );
 }
 
