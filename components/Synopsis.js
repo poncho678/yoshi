78544c7932d4bccd29a8a18b2dfe0817bbb0fff3
poncho678 <vincent.tollens@googlemail.com>
2023-01-14 23:20:38 +0100

    removing logs

--- components/Synopsis.js
+++ components/Synopsis.js
@@ -2,7 +2,6 @@ import styles from "./Synopsis.module.css";
 import { PortableText } from "@portabletext/react";
 
 function Synopsis({ synopsis = [] }) {
-  console.log("synopsis", synopsis);
   return (
     <>
       {synopsis.length > 0 && (
