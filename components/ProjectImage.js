04cc178faa3964363df11fcd3ac112466e7e518c
poncho678 <vincent.tollens@googlemail.com>
2023-01-15 16:59:39 +0100

    fix: if image asset is not available skip component

--- components/ProjectImage.js
+++ components/ProjectImage.js
@@ -3,12 +3,17 @@ import React from "react";
 import { urlFor } from "../server/sanity.server";
 
 function ProjectImage({ image }) {
+  if (!image.asset) {
+    return;
+  }
+
   return (
     <Image
       blurDataURL={image.asset.metadata.lqip}
       placeholder="blur"
       src={urlFor(image).url()}
       alt=""
+      sizes="100vw"
       layout="fill"
       objectFit="cover"
     />
--- components/singleProject/SingleProjectImage.js
+++ components/singleProject/SingleProjectImage.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 import { urlFor } from "../../server/sanity.server";
 
 function SingleProjectImage({ image }) {
+  if (!image.asset) {
+    return;
+  }
+
   return (
     <Image
       src={urlFor(image).url()}
       blurDataURL={image.asset.metadata.lqip}
       placeholder="blur"
-      alt=""
+      alt={image.description ? image.description : ""}
       width="0"
       height="0"
       sizes="100vw"
