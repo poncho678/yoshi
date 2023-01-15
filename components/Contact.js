8fcac35c64b6ce8db38dea455478bb9aed5abc68
poncho678 <vincent.tollens@googlemail.com>
2023-01-15 17:00:03 +0100

    contact spacing is now coherent with the rest of the page

--- components/Contact.js
+++ components/Contact.js
@@ -4,16 +4,22 @@ import styles from "./Contact.module.css";
 function Contact({ infos }) {
   const { mail } = infos;
   return (
-    <section className={styles.contactWrapper}>
-      <hr />
-      <h1>Contact</h1>
+    <section id="contact" className={styles.contactWrapper}>
+      <section>
+        <hr />
+        <h1>Contact</h1>
+      </section>
       <section className={`text-m ${styles.linksWrapper}`}>
         <Link className="btn" href={`mailto:${mail}`}>
           {mail}
         </Link>
         <div className={`text-s ${styles.links}`}>
-          <Link href="/">Data Privacy</Link>
-          <Link href="/">Imprint</Link>
+          <Link className="btn" href="/">
+            Data Privacy
+          </Link>
+          <Link className="btn" href="/">
+            Imprint
+          </Link>
         </div>
       </section>
     </section>
