d56b13a7937ed764515852ec781adf2ef10a35d2
poncho678 <vincent.tollens@googlemail.com>
2023-01-24 20:32:02 +0100

    changed to blue favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
         <link
           rel="icon"
           id="favicon"
-          href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ccircle%20cx=%2250%22%20cy=%2250%22%20r=%2250%22%20fill=%22rgb(250,%2087,%2061)%22/%3E%3C/svg%3E"
+          href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ccircle%20cx=%2250%22%20cy=%2250%22%20r=%2250%22%20fill=%22rgb(000,%2000,%20255)%22/%3E%3C/svg%3E"
         />
       </Head>
       <Component {...pageProps} />
